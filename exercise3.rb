10b5b1704f0b62d22aa414bf84f3ab81396fb846
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-15 15:33:48 -0400

    Gets Age, Calculates Probable Year Born

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,8 @@
 puts "What's yer name?"
-name=gets.chomp
+name=gets.chomp.capitalize
 puts "Hi #{name}!"
+puts "How old are you #{name}?"
+
+age = gets.chomp.to_i
+require "date"
+puts "You were probably born in #{Date.today.year-age}"
