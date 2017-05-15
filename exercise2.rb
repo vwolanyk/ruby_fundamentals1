5ca43ad29ed3c55aba90d2b4896f2da3204cee4f
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-15 15:08:42 -0400

    Complete Exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,17 @@
+# 1. Calculating a tip for $55 meal
+
+bill=55.00
+tip = bill * 0.20
+puts "A good tip for a $#{bill} meal would be $#{tip.round(2)}"
+
+# 2. Integer to String
+
+puts "Converting an integer to a string is as easy as " + 123.to_s
+
+
+# 3. String Interpolation
+
+puts "The answer is: #{45628*7839}"
+
+# 4. True or False
+puts "The answer is #{!!true}"
