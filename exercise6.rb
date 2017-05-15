d233d674693cefcb372c19eccd008bdfa0e1b4b1
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-15 17:08:54 -0400

    Exercise on Exercise 6 up to part 6.1

---
+++ exercise6.rb
@@ -0,0 +1,30 @@
+# Exercise 6 - GET SOME Exercise
+
+# Set Distance Counter
+distance_from_home = 0
+
+# Set variable to continue while loop
+
+want_to_exercise = true
+
+# Looping Exercise
+
+while want_to_exercise
+  puts "Do want to walk or run or go home?"
+  action=gets.chomp.upcase
+
+    if action == "WALK"
+      distance_from_home += 1
+      puts "You are now #{distance_from_home}km from home!"
+
+    elsif action == "RUN"
+    distance_from_home += 5
+    puts "You are now #{distance_from_home}km from home!"
+
+    elsif action == "GO HOME"
+    want_to_exercise = false
+    puts "Hope You enjoyed the fresh air!"
+
+    else puts "Invalid Entry!"
+  end
+end
