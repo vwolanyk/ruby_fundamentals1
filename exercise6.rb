41a2dba6be8c1ee4c297b55fab399f1637a2b70d
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-15 21:39:23 -0400

    Exercise 6.2 Add 'energy' variable

--- exercise6.rb
+++ exercise6.rb
@@ -4,6 +4,9 @@
 # Set Distance Counter
 distance_from_home = 0
 
+# Initial Energy Level
+energy = 10
+
 # Set variable to continue while loop
 want_to_exercise = true
 
@@ -15,14 +18,27 @@ while want_to_exercise
 
     if action == "WALK"
       distance_from_home += 1
-      puts "You are now #{distance_from_home}km from home!"
+      energy += 2
+      puts "You are now #{distance_from_home}km from home and your energy level is #{energy}!"
 
-    elsif action == "RUN"
+    elsif action == "RUN" && energy >= 2
+
     distance_from_home += 5
-    puts "You are now #{distance_from_home}km from home!"
+    energy -= 2
+
+    puts "You are now #{distance_from_home}km from home! Your energy level is #{energy}"
+
+  elsif action == "RUN" && energy < 2
+
+        distance_from_home += 1
+        energy += 2
+
+        puts "These boots were made for walking - You don't have enough energy to run! You are now #{distance_from_home}km from home!"
+
 
     elsif action == "GO HOME"
     want_to_exercise = false
+
     puts "Hope You enjoyed the fresh air!"
 
     else puts "Invalid Entry!"
